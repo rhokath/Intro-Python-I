56742ac9fd066a10327dc24333ca84f2d98eecf2
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-12-03 16:52:15 -0500

    up to cal

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("src/foo.txt", "r") as f:
+    for line in f:
+        print(line, end='')
+    
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("src/bar.text", "w") as f:
+    f.write('this is a test\n')
+
+with open("src/bar.text", "r") as f:
+     for line in f:
+         print(line, end='')
